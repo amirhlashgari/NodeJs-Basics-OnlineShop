dfd0230aeb5d230c648d2bcfae3eb8aff635e37d
amirhoseinlashgari <amirhossein_lashgari@yahoo.com>
2021-11-13 17:35:00 +0330

    refactor: improving Product class model to read and write inside a json file(./data/product.json)

--- models/product.js
+++ models/product.js
@@ -1,4 +1,5 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
 
 module.exports = class Product {
     constructor(title) {
@@ -6,10 +7,26 @@ module.exports = class Product {
     }
 
     save() {
-        products.push(this);
+        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'product.json');
+        fs.readFile(p, (err, content) => {
+            let products = [];
+            if (!err) {
+                products = JSON.parse(content);
+            }
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
     }
 
-    static fetchAll() {
-        return products;
+    static fetchAll(cb) {
+        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'product.json');
+        fs.readFile(p, (err, content) => {
+            if (err) {
+                cb([]);
+            }
+            cb(JSON.parse(content));
+        });
     }
 }
