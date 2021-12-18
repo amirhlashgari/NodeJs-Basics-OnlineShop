cfaeb7fd31bbac16ceea677b2551c5a3f5481843
amirhlashgari <amirhosseinlashgari.al@gmail.com>
2021-12-19 01:04:04 +0330

    feat: adding edit product functionality

--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,7 @@
+const path = require('path');
+
 const express = require('express');
+
 const adminController = require('../controllers/admin');
 
 const router = express.Router();
@@ -11,5 +14,7 @@ router.get('/products', adminController.getProducts);
 
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
+
+router.get('/edit-product/:productId', adminController.getEditProduct);
 
 module.exports = router;
