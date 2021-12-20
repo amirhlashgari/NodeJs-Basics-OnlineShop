f8da796ed0e5780f50a5773155edd248825d855d
amirhlashgari <amirhosseinlashgari.al@gmail.com>
2021-12-20 21:31:56 +0330

    feat: changing product model and its related controllers to work with mysql

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const db = require('./util/database');
 const errorController = require('./controllers/error');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -11,14 +10,6 @@ const res = require('express/lib/response');
 const app = express();
 app.set('view engine', 'ejs');       //to define our templating engine to server
 app.set('views', 'views');           //to show the location of template files
-
-db.execute('SELECT * FROM products')
-    .then(result => {
-        console.log("DATABASEEEEEEEE",result);
-    })
-    .catch(err => {
-        console.log("ERRRRRRRRRRRRRRRRRR",err);
-    });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
