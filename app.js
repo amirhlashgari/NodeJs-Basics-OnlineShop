fa8416461240097f04dff34f7fbbf35edbe20a7f
amirhlashgari <amirhosseinlashgari.al@gmail.com>
2021-12-25 01:24:03 +0330

    feat: definie a new model for user and create its relation with products table

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 const errorController = require('./controllers/error');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -17,6 +19,9 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
 
 sequelize.sync()
     .then(result => {
