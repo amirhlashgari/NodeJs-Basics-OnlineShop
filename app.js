1e515f68a3b61f0cbe99ab772671aa8ad7fc5e92
amirhlashgari <amirhosseinlashgari.al@gmail.com>
2021-12-25 02:44:01 +0330

    feat: define new models and its related relationships

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 const errorController = require('./controllers/error');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -33,6 +35,10 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 //the sequelize syncing will run only once in each server start and create our tables
 sequelize.sync()
