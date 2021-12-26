8f7215010f09ee7e85286377c4ed47ea92d386ab
amirhlashgari <amirhosseinlashgari.al@gmail.com>
2021-12-27 03:19:25 +0330

    refactor: redefine product model due to change in database type

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoConnect = require('./util/database');
+const mongoConnect = require('./util/database').mongoConnect;
 const errorController = require('./controllers/error');
-// const adminRoutes = require('./routes/admin');
-// const shopRoutes = require('./routes/shop');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 app.set('view engine', 'ejs');       //to define our templating engine to server
@@ -13,12 +13,11 @@ app.set('views', 'views');           //to show the location of template files
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/admin', adminRoutes);
-// app.use(shopRoutes);
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(client => {
-    console.log(client);
+mongoConnect(() => {
     app.listen(3000);
 });
