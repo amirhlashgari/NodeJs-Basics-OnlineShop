0382dd1c739e2c7d91a873ac33bc99273e54566c
amirhlashgari <amirhosseinlashgari.al@gmail.com>
2021-12-27 05:42:15 +0330

    refactor: update user model and add cart logic to User class

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoConnect = require('./util/database').mongoConnect;
 const errorController = require('./controllers/error');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const User = require('./models/user');
 
 const app = express();
 app.set('view engine', 'ejs');       //to define our templating engine to server
@@ -12,6 +13,15 @@ app.set('views', 'views');           //to show the location of template files
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    User.findById('61c91956ec7a272dfe8562f3')
+        .then(user => {
+            req.user = new User(user.name, user.email, user.cart, user._id);
+            next();
+        })
+        .catch(err => console.log(err));
+})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
