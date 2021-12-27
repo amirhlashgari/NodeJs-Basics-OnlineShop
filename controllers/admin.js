0be88cde1b7357f912c0fd8789dda78d89432804
amirhlashgari <amirhosseinlashgari.al@gmail.com>
2021-12-27 11:11:51 +0330

    refactor: change product controllers to work with mongoose

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,7 +13,7 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const imageUrl = req.body.imageUrl;
-  const product = new Product(title, price, description, imageUrl, null, req.user._id);
+  const product = new Product({ title: title, price: price, description: description, imageUrl: imageUrl });
   product.save()
     .then(result => {
       res.redirect('/admin/products');
@@ -49,8 +49,14 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedDesc = req.body.description;
   const updatedImageUrl = req.body.imageUrl;
-  const product = new Product(updatedTitle, updatedPrice, updatedDesc, updatedImageUrl, prodId);
-  product.save()
+  Product.findById(prodId)
+    .then(product => {
+      product.titile = updatedTitle;
+      product.price = updatedPrice;
+      product.description = updatedDesc;
+      product.imageUrl = updatedImageUrl;
+      return product.save();
+    })
     .then(result => {
       res.redirect('/admin/products');
     })
@@ -58,7 +64,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then(products => {
       res.render('admin/products', {
         prods: products,
@@ -71,8 +77,8 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId)
-    .then(() => {
+  Product.findByIdAndDelete(prodId)
+    .then(result => {
       res.redirect('/admin/products');
     })
     .catch(err => console.log(err));
