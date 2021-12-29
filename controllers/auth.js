c1c87665662da26b1b2c9c722ebe195be6ca1d3b
amirhlashgari <amirhosseinlashgari.al@gmail.com>
2021-12-30 02:48:02 +0330

    feat: provide logout possibility and delete session

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
     res.render('auth/login', {
         path: '/login',
@@ -7,6 +9,21 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-    req.session.isLoggedIn = true;
-    res.redirect('/');
+    User.findById('61c971150878c9864103ab12')
+        .then(user => {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            req.session.save(err => { // to ensure session is created before redirecting to home page
+                console.log(err);
+                res.redirect('/');
+            });
+        })
+        .catch(err => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+    req.session.destroy(err => {
+        console.log(err);
+        res.redirect('/');
+    });
 };
