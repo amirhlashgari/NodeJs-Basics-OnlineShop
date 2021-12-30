d7932a8e53c1b9641bbfb3fd7ca89d6a042cdb5d
amirhlashgari <amirhosseinlashgari.al@gmail.com>
2021-12-30 16:08:03 +0330

    feat: add reset password functionality to project

--- models/user.js
+++ models/user.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true },
     password: { type: String, required: true },
+    resetToken: String,
+    resetTokenExpiration: Date,
     cart: {
         items: [{
             productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
