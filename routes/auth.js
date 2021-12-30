9febfc1d48dc3ac7fccc0b68a549a9cd161e7565
amirhlashgari <amirhosseinlashgari.al@gmail.com>
2021-12-31 02:06:24 +0330

    feat: add server side validation to signup form

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { check, body } = require('express-validator/check');
 const authController = require('../controllers/auth');
 
 const router = express.Router();
@@ -6,7 +7,21 @@ const router = express.Router();
 router.get('/login', authController.getLogin);
 router.get('/signup', authController.getSignup);
 router.post('/login', authController.postLogin);
-router.post('/signup', authController.postSignup);
+router.post('/signup', [
+    check('email').isEmail().withMessage('Please enter a valid email!').custom((value, { req }) => {
+        if (value === 'test@test.com') {   // adding a custom validator
+            throw new Error('This email address is forbidden');
+        }
+        return true;
+    }),
+    body('password', 'Please enter a password with only nombers and text and at least 5 characters.').isLength({ min: 5 }).isAlphanumeric(),
+    body('confirmPassword').custom((value, { req }) => {
+        if (value !== req.body.password) {
+            throw new Error('Passwords have to match');
+        }
+        return true;
+    })
+], authController.postSignup);
 router.post('/logout', authController.postLogout);
 router.get('/reset', authController.getReset);
 router.post('/reset', authController.postReset);
