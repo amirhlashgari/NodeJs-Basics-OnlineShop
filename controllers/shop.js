4b6f86f3dd7c17d49b3958a5c5f04fa5bf773da0
amirhlashgari <amirhosseinlashgari.al@gmail.com>
2022-01-01 10:31:57 +0330

    feat: add invoice pdf to orders read it from local pre-generated pdf

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const Product = require('../models/product');
 const Order = require('../models/order');
 
@@ -126,6 +128,40 @@ exports.getOrders = (req, res, next) => {
         pageTitle: 'Your Orders',
         orders: orders
       });
+    })
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
+
+exports.getInvoice = (req, res, next) => {
+  const orderId = req.params.orderId;
+  Order.findById(orderId)
+    .then(order => {
+      if (!order) {
+        return next(new Error('No order found'));
+      }
+      if (order.user.userId.toString() !== req.user._id.toString()) {
+        return next(new Error('Unauthorized'));
+      }
+      const invoiceName = 'invoice-' + orderId + '.pdf';
+      const invoicePath = path.join('data', 'invoices', invoiceName);
+      // fs.readFile(invoicePath, (err, data) => {
+      //   if (err) {
+      //     return next();
+      //   }
+      //   res.setHeader('Content-Type', 'application/pdf');
+      //   res.setHeader('Content-Disposition', 'attachment; filename="' + invoiceName + '"');
+      //   res.send(data);
+      // });
+
+      // *** streaming data to improve speed instead of reading it from file system ***
+      const file = fs.createReadStream(invoicePath);
+      res.setHeader('Content-Type', 'application/pdf');
+      res.setHeader('Content-Disposition', 'attachment; filename="' + invoiceName + '"');
+      file.pipe(res);
     })
     .catch(err => {
       const error = new Error(err);
